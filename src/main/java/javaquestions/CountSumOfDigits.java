05f53dc62508d41cb48fd8726ce3d13ee445bdec
tursun0605 <tursun0605@gmail.com>
2022-07-02 21:00:01 +0300

    fisrt commit

---
+++ src/main/java/javaquestions/CountSumOfDigits.java
@@ -0,0 +1,19 @@
+package javaquestions;
+
+import java.util.Scanner;
+
+public class CountSumOfDigits {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please enter a number:");
+        int num=sc.nextInt();
+        int sum=0;
+        while (num!=0){
+            sum=sum+num%10;
+            num=num/10;
+        }
+        System.out.println("the sum of digits is: "+sum);
+    }
+}
